806aeb2156d6ab5dd8602e7795415b4ea7dfe7c2
hansellesteban <hansell.ramos@gmail.com>
2016-09-27 15:57:25 -0500

    Added business logic Added Temperature class Added button actions

--- Convertidor de Temperatura/ViewController.swift
+++ Convertidor de Temperatura/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var temp : Temperature?
 
+    @IBOutlet weak var txtTempC: UITextField!
+    @IBOutlet weak var txtTempF: UITextField!
+    @IBOutlet weak var txtTempK: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        temp = Temperature()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,71 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func convertFromCelcius(_ sender: AnyObject) {
+        txtTempC.text = txtTempC.text == "" ? "0" : txtTempC.text
+        self.temp?.temperature = Double(txtTempC.text!)!
+        self.showValues()
+    }
+    
+    @IBAction func convertFromFarenheit(_ sender: AnyObject) {
+        txtTempF.text = txtTempF.text == "" ? "0" : txtTempF.text
+        self.temp?.fromFarenheit(farenheit: Double(txtTempF.text!)!)
+        self.showValues()
+    }
+    
+    @IBAction func convertFromKelvin(_ sender: AnyObject) {
+        txtTempK.text = txtTempK.text == "" ? "0" : txtTempK.text
+        self.temp?.fromKelvin(kelvin: Double(txtTempK.text!)!)
+        self.showValues()
+    }
+    
+    func showValues() -> Void{
+        txtTempC.text = "\(self.temp!.temperature)"
+        txtTempF.text = "\(self.temp!.toFarenheit())"
+        txtTempK.text = "\(self.temp!.toKelvin())"
+    }
 
+}
 
+class Temperature{
+    
+    open var temperature : Double
+    
+    init(){
+        temperature = 0
+    }
+    
+    convenience init(fromCelcius celcius:Double){
+        self.init()
+        self.temperature = celcius
+    }
+    
+    convenience init(fromFarenheit farenheit:Double){
+        self.init()
+        self.fromFarenheit(farenheit: farenheit)
+    }
+    
+    convenience init(fromKelvin kelvin:Double){
+        self.init()
+        self.fromKelvin(kelvin: kelvin)
+    }
+    
+    public func fromKelvin(kelvin: Double) -> Void{
+        self.temperature = kelvin - 273.15
+    }
+    
+    public func fromFarenheit(farenheit: Double) -> Void{
+        self.temperature = (farenheit-32) * 5 / 9
+    }
+    
+    public func toKelvin() -> Double{
+        return self.temperature + 275.15
+    }
+    
+    public func toFarenheit() -> Double{
+        return (self.temperature * 9 / 5) + 32
+    }
+    
 }
 
